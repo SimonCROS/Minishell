c4b96c9aa77f4f61d308eb9933b5431eb3748701
Simon Cros <scros@student.42lyon.fr>
2021-03-22 16:23:46 +0100

    Examples

--- src/main.c
+++ src/main.c
@@ -1,15 +1,38 @@
 #include "minishell.h"
 
 #include <stdio.h>
+
+int	testFirst(char *element, char *character)
+{
+	return (*element == *character);
+}
+
+void	test(void)
+{
+	int			result;
+	char		*buffer;
+	t_list		*list;
+	char		comp;
+
+	result = 1;
+	list = lst_new(free);
+	while (result)
+	{
+		result = get_next_line(0, &buffer);
+		lst_push(list, buffer);
+	}
+	printf("\n--------------------\n");
+	printf("Taille avant filter %d\n", list->size);
+	lst_filter_in(list, (t_pre)str_not_empty);
+	printf("Taille après filter %d\n", list->size);
+	comp = 't';
+	printf("Premier element qui commence par %c : \"%s\"\n", comp,
+		lst_find_first(list, (t_bipre)testFirst, &comp));
+	lst_destroy(list);
+}
+
 int	main(void)
 {
-	char	*buffer;
-
-	while (1)
-	{
-		get_next_line(0, &buffer);
-		printf("Line : \"%s\"\n", buffer);
-		free(buffer);
-	}
+	test();
 	return (0);
 }
