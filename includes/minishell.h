1c1cb3972325dfb13a817447f0432472f635baf2
Simon Cros <scros@student.42lyon.fr>
2021-04-01 14:02:18 +0200

    Add structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -11,6 +11,9 @@
 
 typedef struct s_gnl_entry	t_gnl_entry;
 
+typedef enum e_command_type	t_command_type;
+typedef struct s_command	t_command;
+
 /*** GNL **********************************************************************/
 
 struct s_gnl_entry
@@ -23,5 +26,26 @@ struct s_gnl_entry
 char		**ft_split_first(char *s, char c);
 int			get_next_line(int fd, char **line);
 int			gnl_init(char ***current, char **tmp_line, ssize_t *result);
+
+/*** Modeles ******************************************************************/
+
+enum e_command_type
+{
+	END,
+	COMMAND,
+	TO_FOUND,
+	PIPE,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+};
+
+struct s_command
+{
+	t_list			args;
+	char			*redirect_in;
+	char			*redirect_out;
+	t_command		parent;
+	t_command_type	next_command_type;
+};
 
 #endif
