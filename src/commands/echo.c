c8fcd834fd7c46ff379426fadbe3d3d0f9fa8fff
Arsene Del sol <adel-sol@z3r7p3.42lyon.fr>
2021-09-13 09:50:18 +0200

    Echo: -n flag fixed

--- src/commands/echo.c
+++ src/commands/echo.c
@@ -1,12 +1,25 @@
 #include "minishell.h"
 
+int	echo_with_n(char *first_arg)
+{
+	if (*first_arg == '-')
+	{
+		++first_arg;
+		while (*first_arg == 'n')
+			++first_arg;
+		if (!*first_arg)
+			return (TRUE);
+	}
+	return (FALSE);
+}
+
 void	do_echo(char **argv)
 {
 	int		endl;
 
 	g_global.cmd_ret = 0;
 	endl = 1;
-	while (*argv && ft_str_equals(*argv, "-n"))
+	while (*argv && echo_with_n(*argv))
 	{
 		argv++;
 		endl = 0;
